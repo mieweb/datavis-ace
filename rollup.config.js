96d033f5917581f53d69c66979bcaea76209e3fe
Taylor Venable <tvenable@mieweb.com>
2024-07-17 12:27:34 -0400

    Dev: Add babel to the build process since we've added libraries (e.g. ChartJS) which aren't IE-mode compatible.

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 //import builtins from 'rollup-plugin-node-builtins';
 //import globals from 'rollup-plugin-node-globals';
+import babel from '@rollup/plugin-babel';
 
 export default {
 	input: 'datavis.js',
@@ -21,5 +22,5 @@ export default {
 			stream: 'undefined',
 		}
 	},
-	plugins: [resolve(), commonjs()/*, builtins(), globals()*/]
+	plugins: [resolve(), commonjs(), /*builtins(), globals(),*/ babel({ babelHelpers: 'bundled' })]
 };
