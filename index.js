6a58e3eeddf4e2a0f238bfcf12f3cc600a080600
Taylor Venable <tvenable@mieweb.com>
2026-04-02 12:17:24 -0400

    Dev: Add exports that GLIDE needs.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-export { deepCopy } from './src/util/deepCopy.js';
+import * as Util from './src/util/misc.js';
+export { Util };
+
 export { OrdMap } from './src/util/ordmap.js';
 export { ParamInput } from './src/source_param.js';
 export { Source, FileSource } from './src/source.js';
@@ -9,7 +11,7 @@ export { PrefsBackend, PREFS_BACKEND_REGISTRY } from './src/prefs_backend.js';
 export { PrefsModule, PREFS_MODULE_REGISTRY } from './src/prefs_module.js';
 export { Perspective } from './src/perspective.js';
 export { Aggregate, AggregateInfo, AGGREGATE_REGISTRY } from './src/aggregates.js';
-export * as Util from './src/util/misc.js';
 export { Lock } from './src/util/lock.js';
 export { types } from './src/types.js';
 export { GroupFunction, GROUP_FUNCTION_REGISTRY } from './src/group_fun.js';
+export { TableExport, Csv } from './src/util/csv.js';
